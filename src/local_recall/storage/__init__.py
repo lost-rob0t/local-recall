c7c780c94c657975cff0fa7491e2f40a05dd3c17
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-18 17:11:43 -0400

    Expose storage failure codes

--- src/local_recall/storage/__init__.py
+++ src/local_recall/storage/__init__.py
@@ -1,5 +1,5 @@
 from .codec import decode_envelope, encode_envelope
-from .errors import StorageFailure
+from .errors import StorageFailure, StorageFailureCode
 from .filesystem import StoragePaths
 from .schema import CURRENT_STORAGE_SCHEMA_VERSION
 from .sqlite_backend import SQLiteEncryptedStorage
@@ -8,6 +8,7 @@ __all__ = [
     "CURRENT_STORAGE_SCHEMA_VERSION",
     "SQLiteEncryptedStorage",
     "StorageFailure",
+    "StorageFailureCode",
     "StoragePaths",
     "decode_envelope",
     "encode_envelope",
