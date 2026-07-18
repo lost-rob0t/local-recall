7c66ebfa5b56fabf9c1dccb1aebe9515f9e655b1
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-17 23:41:02 -0400

    Export strategy protocols

---
+++ src/local_recall/ports/__init__.py
@@ -0,0 +1,42 @@
+"""Backend-neutral strategy interfaces."""
+
+from .capture import CaptureBackend
+from .clock import Clock
+from .encryption import DecryptionRequest, EncryptionProvider, EncryptionRequest
+from .keys import (
+    KeyDestructionRequest,
+    KeyDestructionResult,
+    KeyProvider,
+    KeyRotationRequest,
+)
+from .metadata import MetadataSource
+from .ocr import OCRProvider, OCRRequest
+from .policy import CapturePolicy
+from .providers import EmbeddingProvider, GenerationProvider
+from .redaction import RedactionPolicy, RedactionRequest
+from .routing import ModelRoutingPolicy
+from .storage import DeleteRequest, DeleteResult, StorageBackend
+
+__all__ = [
+    "CaptureBackend",
+    "CapturePolicy",
+    "Clock",
+    "DecryptionRequest",
+    "DeleteRequest",
+    "DeleteResult",
+    "EmbeddingProvider",
+    "EncryptionProvider",
+    "EncryptionRequest",
+    "GenerationProvider",
+    "KeyDestructionRequest",
+    "KeyDestructionResult",
+    "KeyProvider",
+    "KeyRotationRequest",
+    "MetadataSource",
+    "ModelRoutingPolicy",
+    "OCRProvider",
+    "OCRRequest",
+    "RedactionPolicy",
+    "RedactionRequest",
+    "StorageBackend",
+]
