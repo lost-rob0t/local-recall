9f67f17bb04e5a063709c6067cb2875ff22af0fe
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-18 16:57:26 -0400

    Expand storage status codes

--- src/local_recall/storage/errors.py
+++ src/local_recall/storage/errors.py
@@ -3,6 +3,11 @@ from enum import StrEnum
 
 class StorageFailureCode(StrEnum):
     INVALID_TYPE = "invalid_type"
+    INVALID_RECORD_ID = "invalid_record_id"
+    UNSAFE_ROOT = "unsafe_root"
+    DUPLICATE_RECORD = "duplicate_record"
+    QUOTA_EXCEEDED = "quota_exceeded"
+    BLOB_TOO_LARGE = "blob_too_large"
 
 
 class StorageFailure(RuntimeError):
