422a8ca44fc85f0641773742b17507ee9b34f19a
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-18 17:10:22 -0400

    Export SQLite storage backend

--- src/local_recall/storage/__init__.py
+++ src/local_recall/storage/__init__.py
@@ -1,4 +1,12 @@
 from .codec import decode_envelope, encode_envelope
 from .filesystem import StoragePaths
+from .schema import CURRENT_STORAGE_SCHEMA_VERSION
+from .sqlite_backend import SQLiteEncryptedStorage
 
-__all__ = ["StoragePaths", "decode_envelope", "encode_envelope"]
+__all__ = [
+    "CURRENT_STORAGE_SCHEMA_VERSION",
+    "SQLiteEncryptedStorage",
+    "StoragePaths",
+    "decode_envelope",
+    "encode_envelope",
+]
