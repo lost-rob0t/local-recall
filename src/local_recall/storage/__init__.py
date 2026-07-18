860effab78c30739a10da5f20b5f43676d5a7223
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-18 17:10:14 -0400

    Export storage paths

--- src/local_recall/storage/__init__.py
+++ src/local_recall/storage/__init__.py
@@ -1,3 +1,4 @@
 from .codec import decode_envelope, encode_envelope
+from .filesystem import StoragePaths
 
-__all__ = ["decode_envelope", "encode_envelope"]
+__all__ = ["StoragePaths", "decode_envelope", "encode_envelope"]
